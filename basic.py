b9d8391b98b569d53f2e5919e5613f4c0be9832d
Nida Fatima <nidaf2686@gmail.com>
2026-03-03 15:09:11 +0500

    this is phyton

---
+++ basic.py
@@ -0,0 +1,33 @@
+print("hi")
+#comments
+#this code is replacing empty spces in the text
+
+#variables understanding
+user_age = 22
+User_age = 44
+
+user_age = 33
+a = 12
+age = 12
+student_age = 20
+
+
+# name = "zaheen" 
+
+
+
+print(user_age,User_age)
+
+## data types 
+#Numeric
+# integer: 23424, -3,0,-234
+# float:234.098, -23.0, -1.0
+#text
+# string: "we are learning datatypes", 'abc garden town', '''text''',"""text"""
+#boolean: True,False
+
+##collections
+list: cloud_class_students = ["ali", "hamza","abc","aaa"]
+tuple:  cloud_class = ("ali", "hamza","abc","aaa")
+Dictionary:student_data = {'name':"ali",'age':17,"address":"abc street multan"}
+set:{12,14,12}
